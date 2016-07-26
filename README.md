35fb9a599197e7d8cb19d06cf47e65c3a954bbe0
wenzhang <zhangwenting111@gmail.com>
2016-07-26 23:46:26 +0800

    class 04

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Color Generator 03
+Color Generator 04
 =====
-randomly pick one color, and generate gradient based on 1) hue and 2)saturation or lightnes
+randomly pick one color, and generate gradient based on 1) hue and 2)saturation or lightnes, use two numbers as params.
--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(function() {
-	generateLevel(10);
+	generateLevel(9,6);
 })
 
-function generateLevel(colorTotal) {
-	var colorTotal = colorTotal;
+function generateLevel(colorTotal1, colorTotal2) {
+	var colorTotal1 = colorTotal1;
+	var colorTotal2 = colorTotal2;
 	var color1 = new THREE.Color(Math.random(), Math.random(), Math.random());
 	var color1Hue = color1.getHSL().h;
 	var color1Saturation = color1.getHSL().s;
@@ -19,8 +20,8 @@ function generateLevel(colorTotal) {
 	// var color2Hue = color1Hue > 0.5?(color1Hue-0.5):(color1Hue+0.5)
 	var color2 = color1.clone().setHSL(color2Hue, color1Saturation, color1Lightness);
 
-	for (k = 0; k < colorTotal; k++) {
-  	var tempColor = color1.clone().lerp(color2, k / (colorTotal - 1));
+	for (i = 0; i < colorTotal1; i++) {
+  	var tempColor = color1.clone().lerp(color2, i / (colorTotal1 - 1));
 		var newRectangle = $("<div></div>")
 												.css('background-color', tempColor.getStyle());
     $("#game").append(newRectangle);
@@ -31,8 +32,8 @@ function generateLevel(colorTotal) {
 	var color2Lightness = color2.getHSL().l;
   var color3 = color2.clone().setHSL(color2Hue, color2Saturation, 0.2);
 
-	for (k = 1; k < colorTotal; k++) {
-  	var tempColor = color2.clone().lerp(color3, k / (colorTotal - 1));
+	for (j = 1; j < colorTotal2; j++) {
+  	var tempColor = color2.clone().lerp(color3, j / (colorTotal2 - 1));
 		var newRectangle = $("<div></div>")
 												.css('background-color', tempColor.getStyle());
     $("#game").append(newRectangle);
